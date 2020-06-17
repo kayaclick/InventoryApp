ad54d71ce356a3097133edfdf3aec9bf88818048
Kaya Click <kclick@spscards.net>
2020-06-16 23:11:27 -0500

    Export CSV working.

--- inventoryApp/ViewControllers/ExportViewController.swift
+++ inventoryApp/ViewControllers/ExportViewController.swift
@@ -14,6 +14,7 @@ class ExportViewController: UIViewController {
     @IBOutlet weak var backgroundView: UIView!
     @IBOutlet weak var docCountLbl: UILabel!
     
+    @IBOutlet weak var exportCSV: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor       = staticVars().accentColour
@@ -31,6 +32,32 @@ class ExportViewController: UIViewController {
         
     }
     
+    @IBAction func exportCSVPressed(_ sender: Any) {
+        exportData()
+    }
+    func exportData() {
+        let fileName = "Inventory.csv"
+        var csvText = "SKU,qty,Name,imageURL,brand,asin,elid\n"
+        let index = DBHelper().getDocList()
+        
+        for docid in index { //Iterate doc list and pull doc info
+            let doc = DBHelper().getDoc(docid as! String)
+            let tempString = "\(doc.sku),\(doc.qty),\(doc.name),\(doc.imageURL),\(doc.brand),\(doc.asin),\(doc.elid)\n"
+            csvText.append(tempString)
+            
+        }
+        
+        guard let path = try? FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false).appendingPathComponent(fileName) as NSURL
+            else {
+                return
+            }
+        
+        do {
+            try csvText.write(to: path as URL, atomically: true, encoding: String.Encoding.utf8)
+        } catch {
+            print("Failed to write csv!") //toast here
+        }
+    }
     
     //Goodbye
     @IBAction func backButtonPressed(_ sender: Any) {
